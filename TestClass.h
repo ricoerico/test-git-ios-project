18e0beb04a3766517c0056c338cb999aeef71882
Rico <rico@fm-dyn-118-137-107-93.fast.net.id>
2014-08-08 19:14:41 +0700

    added a test class

---
+++ TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  Git Test
+//
+//  Created by Rico on 8/8/14.
+//  Copyright (c) 2014 Secret Society Softwares. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  Git Test
+//
+//  Created by Rico on 8/8/14.
+//  Copyright (c) 2014 Secret Society Softwares. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
